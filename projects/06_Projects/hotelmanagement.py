f75434872c850b10951521bb7932b241028d7e88
jayden0511 <keepursmile@gmail.com>
2024-11-30 19:32:38 +0000

    day 16 makeup class: we technically finished

--- projects/06_Projects/hotelmanagement.py
+++ projects/06_Projects/hotelmanagement.py
@@ -13,20 +13,40 @@ from tkinter import messagebox, simpledialog, Tk
 window = Tk()     
 window.withdraw()
 
-db = {}
+guest = {}
 
-room_numbers = ("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20")
+db = {} 
+
+level_floors = ("1 2 3 4 5 6 7 8")
+room_numbers = ("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20")
 
 def i():
     ask = simpledialog.askstring("Hotel management" , "What is your name?")
     check = simpledialog.askstring("Hotel management", "Would you like to check in or out?")
     if check == "check in" or check == "in":
         chosen_rooms = []
-        room_number = simpledialog.askstring("Hotel management", room_numbers)
+        messagebox.showinfo()
+        level_floor = simpledialog.askinteger("Hotel management", level_floors)
+        if level_floor >8:
+            print("Error")
+            return
+        room_number = simpledialog.askinteger("Hotel management", room_numbers)
+        if room_number >20:
+            print("Error")
+            return
         guests = simpledialog.askinteger("Hotel management", "How many guests are you traveling with?")
         nights = simpledialog.askinteger("Hotel management", "How many nights are staying?")
-        price = nights*90
+        price = nights*200
+        messagebox.showinfo("Your cost is:", price)
+        guest.add(Key,Value)
+        Dict_
     elif check == "check out" or check == "out":
         messagebox.showinfo("Hotel management", "Thank you for your time")
         messagebox.showinfo("Your cost is:", price)
-    db =[] 
+        guest.remove(room_number)
+    dictionary = {
+        ask, level_floor, room_number,
+
+     }
+        
+i()
