0f97ac0a6a1a44a29cf9de49e9e8597a3955e0e2
jayden0511 <keepursmile@gmail.com>
2024-12-10 00:59:01 +0000

    day 17 RAAAAAAAAAH im technically done? :3

--- projects/06_Projects/hotelmanagement.py
+++ projects/06_Projects/hotelmanagement.py
@@ -20,33 +20,37 @@ db = {}
 level_floors = ("1 2 3 4 5 6 7 8")
 room_numbers = ("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20")
 
-def i():
-    ask = simpledialog.askstring("Hotel management" , "What is your name?")
-    check = simpledialog.askstring("Hotel management", "Would you like to check in or out?")
-    if check == "check in" or check == "in":
-        chosen_rooms = []
-        messagebox.showinfo()
-        level_floor = simpledialog.askinteger("Hotel management", level_floors)
-        if level_floor >8:
-            print("Error")
-            return
-        room_number = simpledialog.askinteger("Hotel management", room_numbers)
-        if room_number >20:
-            print("Error")
-            return
-        guests = simpledialog.askinteger("Hotel management", "How many guests are you traveling with?")
-        nights = simpledialog.askinteger("Hotel management", "How many nights are staying?")
-        price = nights*200
-        messagebox.showinfo("Your cost is:", price)
-        guest.add(Key,Value)
-        Dict_
-    elif check == "check out" or check == "out":
-        messagebox.showinfo("Hotel management", "Thank you for your time")
-        messagebox.showinfo("Your cost is:", price)
-        guest.remove(room_number)
-    dictionary = {
-        ask, level_floor, room_number,
-
-     }
+def i(db):
+    while True:
+        print(db)
+        ask = simpledialog.askstring("Hotel management" , "What is your name?")
+        check = simpledialog.askstring("Hotel management", "Would you like to check in, out or quit?")
+        if check == "check in" or check == "in":
+            chosen_rooms = []
+            level_floor = simpledialog.askinteger("Hotel management", level_floors)
+            if level_floor >8:
+                print("Error")
+                return
+                room_func(room_numbers)
+            guests = simpledialog.askinteger("Hotel management", "How many guests are you traveling with?")
+            nights = simpledialog.askinteger("Hotel management", "How many nights are staying?")
+            price = nights*200
+            messagebox.showinfo("Your cost is:", price)
+            db[ask] = chosen_rooms
+        elif check == "check out" or check == "out":
+            messagebox.showinfo("Hotel management", "Thank you for your time")
+            messagebox.showinfo("Your cost is:", price)
+            db.pop[ask]
+        elif check == "quit" or check == "quit":
+            break
+def room_func(room_numbers, chosen_rooms):
+    room_number = simpledialog.askinteger("Hotel management", room_numbers)
+    chosen_rooms.append(room_number)
+    if room_number in room_numbers:
+        room_numbers.remove(room_number)
+        if room_number >20 or room_number in chosen_rooms:
+                messagebox.showerror("Error")
+                return
         
-i()
+    return room_number
+i(db)
