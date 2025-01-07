c20c8abb46d4dd25a5343e572775360f0d07a8c0
jayden0511 <keepursmile@gmail.com>
2025-01-07 00:40:40 +0000

    day 20 I FINALLY DONE WITH DA PEROJECT

--- projects/06_Projects/hotelmanagement.py
+++ projects/06_Projects/hotelmanagement.py
@@ -15,7 +15,8 @@ window.withdraw()
 
 guest = {}
   
-db = {} 
+db = {}
+
 
 level_floors = ("1 2 3 4 5 6 7 8")
 room_numbers = ("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20")
@@ -25,8 +26,9 @@ def i(db):
         print(db)
         ask = simpledialog.askstring("Hotel management" , "What is your name?")
         check = simpledialog.askstring("Hotel management", "Would you like to check in, out or quit?")
-        room_number = simpledialog.askinteger("Hotel management", room_numbers)
+       
         if check == "check in" or check == "in":
+            room_number = simpledialog.askinteger("Hotel management", room_numbers) 
             chosen_rooms = [room_number]
             if room_number >20 or room_number <1:
                print("Error")
@@ -40,14 +42,17 @@ def i(db):
             if guests >4 or guests <1:
                 simpledialog.askinteger("Hotel management", "ERROR, how many rooms would you like to book?")
             nights = simpledialog.askinteger("Hotel management", "How many nights are you staying?")
+            room_service = messagebox.showinfo("Hotel management", "By the way dial 1 on the telephone to call room service")
             price = nights*200
-            messagebox.showinfo("Your cost is:", price)
+            messagebox.showinfo("Hotel management", price)
             db[ask] = chosen_rooms
         elif check == "check out" or check == "out":
             messagebox.showinfo("Hotel management", "Thank you for your time")
-            messagebox.showinfo("Your cost is:", price)
-            db.pop[ask, chosen_rooms]
+            db.pop(ask, chosen_rooms)
         elif check == "quit" or check == "quit":
+            break
+        else:
+            messagebox.showerror("Hotel management", "Answer was not informative, please try again")
             break
 def room_func(room_numbers, chosen_rooms):
     room_number = simpledialog.askinteger("Hotel management", room_numbers)
